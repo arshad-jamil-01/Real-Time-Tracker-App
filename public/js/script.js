06de99ce4b745e31362e325183f65c9637c52357
Arshad Jamil <arshadjamil533@gmail.com>
2025-04-19 17:33:50 +0530

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,68 @@
+//const socket = io(); // ye code mera hai
+
+//ye code chat gpt ka hai
+let myId;
+
+const socket = io();
+
+socket.on("connect", () => {
+  myId = socket.id;
+});  ///yaha tak code chat gpt ka hai
+
+
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+       const {latitude, longitude} = position.coords;
+       socket.emit("send-location", {latitude, longitude});
+
+    }, (error)=>{
+        console.error(error)
+    },
+    {
+        enableHighAccuracy:true,
+        timeout: 5000,
+        maximumAge:0,
+    }
+);
+}
+
+const map = L.map("map").setView([0, 0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+
+const marker = {};
+
+socket.on("recieve-location", (data)=>{
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude]);
+
+    if(marker[id]){
+        marker[id].setLatLng([latitude, longitude])
+    }else{
+        marker[id] = L.marker([latitude, longitude]).addTo(map)
+    }
+})
+
+socket.on("user-disconnected", (id)=>{
+
+
+//ye code mera hai
+// if(marker[id]){
+//     map.removeLayer(marker[id])
+//     delete marker[id]
+// }
+
+
+//ye code chat gpt ka hai
+if (id !== myId && marker[id]) {
+    map.removeLayer(marker[id]);
+    delete marker[id];
+  }
+
+
+})
